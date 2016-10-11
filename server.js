38f6380af181ec8fe2d5bd1ff725aa370fb470ac
ramvar <ramvar476@gmail.com>
2016-10-11 11:18:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/article-two', function(req,res) {
     res.send("article-two requested");
 });
 
-app.get('/article-one', function(req,res) {
+app.get('/article-three', function(req,res) {
     res.send("article-three requested");
 });
 
